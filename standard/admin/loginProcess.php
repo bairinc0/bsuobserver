6839066828331ef7828899fdd3ccb4ad14e3f4b8
Zeyder <pumpingmonster04b63@gmail.com>
2013-09-30 10:40:37 +0900

    Небольшие изменения в проверки формы.Запилен под Ajax

--- standard/admin/loginProcess.php
+++ standard/admin/loginProcess.php
@@ -7,24 +7,32 @@ $Site_Name="";
 //----------------------------Подключаем библиотеки-------------------------------------
 require_once($_SERVER['DOCUMENT_ROOT'].$Site_Name.'/library/db/db.php');
 require_once($_SERVER['DOCUMENT_ROOT'].$Site_Name.'/library/auth/auth.php');
-require_once($_SERVER['DOCUMENT_ROOT'].$Site_Name.'/library/smarty/Smarty.class.php');
+/*require_once($_SERVER['DOCUMENT_ROOT'].$Site_Name.'/library/smarty/Smarty.class.php');
 //--------------------------Подключаем Smarty-----------------------------------------
+
 $smarty = new Smarty();
 $smarty->template_dir = 'template/';
 $smarty->compile_dir = $_SERVER['DOCUMENT_ROOT'].$Site_Name.'/library/smarty/templates_c/';
 $smarty->config_dir = $Site_Name.'/library/smarty/configs/';
 $smarty->cache_dir = $Site_Name.'/library/smarty/cache/';
+*/
 //--------------------------Авторизация пользователя----------------------------------
 $login=$_POST['login'];
 $password=$_POST['password'];
 $user=login($login,$password);
 if ($user){
 	setUser($user);
-	$smarty->assign('login',$_SESSION['logged_login']);
+	header("Location:/cabinet");
+	/*$smarty->assign('login',$_SESSION['logged_login']);
 	$smarty->assign('status',$_SESSION['status']);	
-	$smarty->display($_SERVER['DOCUMENT_ROOT'].'/standard/admin/template/loginSuccess.tpl');}
+	$smarty->display($_SERVER['DOCUMENT_ROOT'].'/standard/admin/template/loginSuccess.tpl');
+	
+		//echo "Здравствуйте, ".$_SESSION['logged_login'].". Вы ".$_SESSION['status'].". <a href='/cabinet'>Личный кабинет</a>";
+	*/
+	}
+	
 else{
-	$smarty->display($_SERVER['DOCUMENT_ROOT'].'/standard/admin/template/loginFail.tpl');
+	echo 0;
 }
 
 
